37cdcb83de66551a67ddd80caf9c0d44ff9610e4
Šimon Dočkal <dockals.04@spst.cz>
2022-04-28 10:53:10 +0200

    Přidejte soubory projektu.

---
+++ P02/Form1.cs
@@ -0,0 +1,60 @@
+﻿using poleUtil;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text != "")
+            {
+
+                int n = Convert.ToInt32(textBox1.Text);
+                int[] pole;
+                if (textBox2.Text != "" && textBox3.Text != "")
+                {
+                    int prvni = Convert.ToInt32(textBox2.Text);
+                    int druhe = Convert.ToInt32(textBox3.Text);
+                    pole = Pole.generate(n, prvni, druhe);
+
+                }
+                else
+                {
+                    pole = Pole.generate(n);
+                }
+                Pole.zobrazeni(listBox1, pole);
+                int pocty = Pole.pocets(pole, out int licha);
+                label4.Text = "pocet lichych cisel je " + licha;
+                label5.Text = "soucet sudych cisel je " + pocty;
+                bool ros = Pole.rostouci(pole);
+                if (!ros)
+                {
+                    label7.Text = "Pole neni rostouci";
+                }
+                else
+                {
+                    label7.Text = "Pole je roustouci";
+                }
+                Pole.vymena(pole);
+                Pole.zobrazeni(listBox2, pole);
+            }
+        }
+    }
+}
---
+++ metodypole/Pole.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace poleUtil
+{
+    public class Pole
+    {
+
+        /// <summary>
+        /// generuje nahodne cisla
+        /// </summary>
+        /// <param name="n">delka pole</param>
+        /// <param name="from">od jakeho cisla zacina generovani</param>
+        /// <param name="to">u jakeho cisla konci generovani</param>
+        /// <returns>vraci pole nahodnych prvku int</returns>
+        static public int[] generate(int n, int from = 1, int to = 100)
+        {
+
+            int[] pole = new int[n];
+            Random rr = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                pole[i] = rr.Next(from, to);
+            }
+            return pole;
+        }
+        /// <summary>
+        /// metoda na zobrazeni prvku do listboxu
+        /// </summary>
+        /// <param name="list">listbox do ktereho se ukladaji prvky</param>
+        /// <param name="pole">pole prvku</param>
+        static public void zobrazeni(ListBox list, int[] pole)
+        {
+            list.Items.Clear();
+            for (int i = 0; i < pole.Length; i++)
+            {
+                list.Items.Add(pole[i]);
+            }
+        }
+
+        /// <summary>
+        /// pocet sudych cisel a soucet lichych cisel
+        /// </summary>
+        /// <param name="pole">pole prvku</param>
+        /// <param name="n">delka pole</param>
+        /// <param name="lich">vraci soucet lichych cisel</param>
+        /// <returns>vraci soucet lichych cisel</returns>
+        static public int pocets(int[] pole, out int lich)
+        {
+            int pocet = 0;
+            lich = 0;
+            for (int i = 0; i < pole.Length; i++)
+            {
+                if (pole[i] % 2 == 0)
+                {
+                    pocet += pole[i];
+                }
+                else
+                {
+                    lich++;
+                }
+            }
+            return pocet;
+        }
+
+        /// <summary>
+        /// zjistuje jestli je pole roustouci
+        /// </summary>
+        /// <param name="pole">pole prvku</param>
+        /// <returns>vraci jestli je pole rostouci nebo ne</returns>
+        static public bool rostouci(int[] pole)
+        {
+
+            for (int i = 0; i < pole.Length - 1; i++)
+            {
+                if (pole[i] > pole[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// vynenuje nejvetsi prvek s poslednim
+        /// </summary>
+        /// <param name="pole">pole prvku</param>
+        static public void vymena(int[] pole)
+        {
+            int max = int.MinValue;
+            int pozice = 0;
+            for (int i = 0; i < pole.Length; i++)
+            {
+                if (pole[i] > max)
+                {
+                    max = pole[i];
+                    pozice = i;
+                }
+            }
+            pole[pozice] = pole[pole.Length - 1];
+            pole[pole.Length - 1] = max;
+        }
+    }
+}
