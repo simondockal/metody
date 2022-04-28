fd656048554d033753807a890a606dc3b94cd6bf
Šimon Dočkal <dockals.04@spst.cz>
2022-04-28 11:19:06 +0200

    P03

--- P03/Form1.cs
+++ P03/Form1.cs
@@ -21,7 +21,27 @@ namespace P03
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            if(textBox1.Text!=""&&textBox2.Text!="")
+            {
+                int a = Convert.ToInt32(textBox1.Text);
+                int b = Convert.ToInt32(textBox2.Text);
+                int obsah;
+                double uhlopricka;
+                if(a==b)
+                {
+                    obsah = a * a;
+                    uhlopricka = Math.Sqrt(a * a + a * a);
+                    label3.Text = "Uhlopricka ctverce je " + uhlopricka.ToString("F2");
+                    label4.Text = "Obsah ctverce je " + obsah;
+                }
+                else
+                {
+                    obsah = a*b;
+                    uhlopricka = Math.Sqrt(a * a + b * b);
+                    label3.Text = "Uhlopricka obdelniku je " + uhlopricka.ToString("F2");
+                    label4.Text = "Obsah obdelniku je " + obsah;
+                }
+            }
         }
     }
 }
